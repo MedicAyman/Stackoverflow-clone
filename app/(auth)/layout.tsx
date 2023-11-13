11bdfc473ceee843f6cd90831f7e1bcd31b07c15
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2023-11-13 02:20:18 +0200

    add auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import React, { PropsWithChildren } from 'react';
+
+const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+	return (
+		<main className="flex min-h-screen w-full items-center justify-center">
+			{children}
+		</main>
+	);
+};
+export default Layout;
